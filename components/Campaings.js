eb0cd47183260c0406c54d740a5efa5193bd342c
furkanugurlu <furkanugurlu3451@gmail.com>
2022-08-28 19:17:26 +0300

    page structures created

---
+++ components/Campaings.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Campaings() {
+  return <div>Campaings</div>;
+}
+
+export default Campaings;
---
+++ components/index.js
@@ -0,0 +1,8 @@
+export { default as Header } from "./Header";
+export { default as Campaings } from "./Campaings";
+export { default as Cards } from "./Cards";
+export { default as Categories } from "./Categories";
+export { default as Favorites } from "./Favorites";
+export { default as HeroSection } from "./HeroSection";
+export { default as Footer } from "./Footer";
+export { default as MobileApp } from "./MobileApp";
